586719f595d485fcf9d7876aac6c442747c38bbb
Cory Taylor <cory.c.taylor@gmail.com>
2012-04-13 22:40:51 -0400

    initial commit

---
+++ ThreeAndAHalfMinutes/Controllers/TwilioAppController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using Twilio.TwiML.Mvc;
+
+namespace ThreeAndAHalfMinutes.Controllers
+{
+    public class TwilioAppController : Controller
+    {
+        //
+        // GET: /TwilioApp/
+
+        public ActionResult Receive()
+        {
+            var twiml = new Twilio.TwiML.TwilioResponse();
+            twiml.Say("Hello Monkey!");
+
+            return new TwiMLResult(twiml);
+        }
+
+    }
+}
